fdd815eec9d56b8b805d125ab49ae3f801838629
Seth Etter <sethetter@gmail.com>
2021-10-19 21:15:43 -0500

    Different method of setting HTTPS in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->environment() != 'local') {
-            URL::forceScheme('https');
+            $this->server->set('HTTPS', true);
         }
     }
 }
