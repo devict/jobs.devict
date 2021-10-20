0fa708841b58ddf2c4902adf4086cfae37a89880
Seth Etter <655500+sethetter@users.noreply.github.com>
2021-10-19 21:48:31 -0500

    Fix signed job edit links (#37)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment() != 'local') {
+        if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
     }
